c19e4c2f95394da29d2043543648070a0a98f43a
Ram <r4mbhardwaj@gmail.com>
2023-10-16 02:10:45 +0530

    feat: Implement basic compression and decompression
    Co-authored-by: Ram <r4mbhardwaj@gmail.com>
    Co-authored-by: Ram <8dspan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
-fn store(filename: String) {
-    println!("storing: {}", filename);
-}
+extern crate flate2;
 
-fn load(filename: String) {
-    println!("loading: {}", filename);
+use flate2::{bufread::GzDecoder, write::GzEncoder, Compression};
+use std::fs::File;
+use std::io::{copy, BufReader, BufWriter};
+
+pub struct ByteTheater {}
+
+const EXTENSION: &str = ".gz";
+
+impl ByteTheater {
+    pub fn compress(movie_path: &str, compressed_path: &str) -> std::io::Result<()> {
+        let movie_file = File::open(movie_path)?;
+        let mut reader = BufReader::new(movie_file);
+
+        let compressed_file = File::create(compressed_path)?;
+        let encoder = GzEncoder::new(compressed_file, Compression::default());
+
+        let mut writer = BufWriter::new(encoder);
+        copy(&mut reader, &mut writer)?;
+
+        Ok(())
+    }
+
+    pub fn decompress(compressed_path: &str, decompressed_path: &str) -> std::io::Result<()> {
+        let compressed_file = File::open(compressed_path)?;
+        let reader = BufReader::new(compressed_file);
+        let mut decoder = GzDecoder::new(reader);
+
+        let decompressed_file = File::create(decompressed_path)?;
+        let mut writer = BufWriter::new(decompressed_file);
+        copy(&mut decoder, &mut writer)?;
+
+        Ok(())
+    }
 }
 
 fn main() {
+    println!("ByteTheater: A video compression tool written in Rust.");
+
     let args = std::env::args().collect::<Vec<String>>();
     println!("args: {:?}", args);
 
@@ -19,25 +50,35 @@ fn main() {
     let filename = args[2].clone();
 
     match mode.as_str() {
-        "store" => {
-            if args.len() < 3 {
-                println!("Please Please provide a file to store");
-                return;
-            }
-            println!("store mode");
-            store(filename);
+        "dump" => {
+            println!("Compressing {} to {}{} ...", filename, filename, EXTENSION);
+            ByteTheater::compress(&filename, &format!("{}{}", filename, EXTENSION)).unwrap();
         }
         "load" => {
-            if args.len() < 3 {
-                println!("Please provide the stored file to load");
-                return;
-            }
-            println!("load mode");
-            load(filename);
+            let filename = filename.trim_end_matches(EXTENSION);
+            println!(
+                "Decompressing {}{} to {} ...",
+                filename, EXTENSION, filename
+            );
+            println!("{}{}", filename, EXTENSION);
+            println!("{}", filename);
+
+            let folder_path = filename.split("/").collect::<Vec<&str>>();
+            let exact_file_name = format!("Output_{}", folder_path[&folder_path.len() - 1]);
+
+            let output_path = format!("{}/{}", folder_path[0], exact_file_name);
+            println!("output_path: {}", output_path);
+            ByteTheater::decompress(
+                &format!("{}{}", filename, EXTENSION),
+                &format!("{}", output_path),
+            )
+            .unwrap();
         }
         _ => {
-            println!("unknown mode; must be 'store' or 'load'");
+            println!("Usage: {} <filename>", args[0]);
             std::process::exit(1);
         }
     }
+
+    /* Streaming method usage ... */
 }
